6d2881268ee820c0b783af958f77b3e4ce45ea4a
Evandro Costa <evandro.souzac@gmail.com>
2024-08-02 14:47:54 -0300

    chore: install `tailwindcss` to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      yellow: "#ffba03",
+      "yellow-light": "#ffdd81",
+      gray: "#b8b8b8",
+      "gray-light": "#f3f3f2",
+      green: "#7e9c3d",
+      "green-light": "#1cbaa6",
+      red: "#d92336",
     },
   },
   plugins: [],
