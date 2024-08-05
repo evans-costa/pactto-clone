9321c70704acba4205b85bfa7c812896a0c8bc18
Evandro Costa <evandro.souzac@gmail.com>
2024-08-05 17:51:05 -0300

    style: add `gray-dark` color and extends theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    colors: {
-      yellow: "#ffba03",
-      "yellow-light": "#ffdd81",
-      gray: "#b8b8b8",
-      "gray-light": "#f3f3f2",
-      green: "#7e9c3d",
-      "green-light": "#1cbaa6",
-      red: "#d92336",
+    extend: {
+      colors: {
+        yellow: "#ffba03",
+        "yellow-light": "#ffdd81",
+        gray: "#b8b8b8",
+        "gray-dark": "#2f2f2f",
+        "gray-light": "#f3f3f2",
+        green: "#7e9c3d",
+        "green-light": "#1cbaa6",
+        red: "#d92336",
+      },
     },
   },
   plugins: [],
