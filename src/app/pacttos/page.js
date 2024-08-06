7b4bfa4614a2fd776967c719724cb5dc29b48125
Evandro Costa <evandro.souzac@gmail.com>
2024-08-06 14:09:15 -0300

    feat: create `/pacttos` page

---
+++ src/app/pacttos/page.js
@@ -0,0 +1,13 @@
+import Input from "@/components/Input";
+import Section from "@/components/Section";
+
+export default function Pacttos() {
+  return (
+    <Section>
+      <h2 className="mt-8 text-4xl">Pacttos (Chats)</h2>
+      <div className="mt-12 w-full">
+        <Input />
+      </div>
+    </Section>
+  );
+}
