2228ff0879bdfa97dd4f0a3bb5e9b33386fdc80f
Evandro Costa <evandro.souzac@gmail.com>
2024-08-08 14:58:08 -0300

    refactor(component): using rest operator on props  attribute to make component more clean

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,18 +1,16 @@
-import SearchIcon from "@mui/icons-material/Search";
-
-export default function Input() {
+export default function Input({ icon, ...props }) {
   return (
     <>
-      <label className="sr-only" htmlFor="search"></label>
+      <label className="sr-only" htmlFor={props.id}></label>
       <div className="relative">
         <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-4">
-          <SearchIcon className="h-7 w-7 text-white" />
+          {icon ? icon : null}
         </div>
         <input
-          id="search"
-          placeholder="Search"
+          id={props.id}
+          placeholder={props.placeholder}
           type="text"
-          className="w-full rounded-md bg-transparent p-4 pe-14 font-medium text-white outline-none hover:border-gray-light hover:ring-gray-light focus:border-yellow focus:ring-yellow"
+          className="w-full rounded-md bg-transparent p-4 pe-14 font-medium text-white outline-none placeholder:text-lg placeholder:text-white/50 hover:border-gray-light hover:ring-gray-light focus:border-yellow focus:ring-yellow"
         />
       </div>
     </>
