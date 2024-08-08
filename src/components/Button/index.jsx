46688566c82543f66c2a65007bf972ac988ced15
Evandro Costa <evandro.souzac@gmail.com>
2024-08-08 14:29:44 -0300

    refactor(component): using rest operator on `Button` component props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,11 @@
-export default function Button({ icon, label }) {
+export default function Button({ background, ...props }) {
   return (
-    <button className="rounded-3xl bg-yellow px-4 py-4 text-base uppercase">
+    <button
+      type={props.type}
+      form={props.form}
+      className={`rounded-3xl bg-${background} px-6 py-4 text-base uppercase`}>
       <span className="flex items-center justify-center gap-2">
-        {icon ? icon : ""} {label}
+        {props.icon ? props.icon : ""} {props.label}
       </span>
     </button>
   );
