f97ad4f3e86ce44f3450b9886a3941f044ec57f6
Evandro Costa <evandro.souzac@gmail.com>
2024-08-12 14:35:56 -0300

    refactor: refactor `FormDropdown` component and changes on Profile Form

--- src/components/Forms/FormDropdown/index.jsx
+++ src/components/Forms/FormDropdown/index.jsx
@@ -1,40 +1,15 @@
-"use client";
-
-import { fetchCountries } from "@/utils/fetchCountries";
-import {
-  Listbox,
-  ListboxButton,
-  ListboxOption,
-  ListboxOptions,
-} from "@headlessui/react";
+import { Listbox, ListboxButton, ListboxOptions } from "@headlessui/react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { useEffect, useState } from "react";
 
-export default function FormDropdown() {
-  const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState("");
-
-  useEffect(() => {
-    const getCountries = async () => {
-      const countryNames = await fetchCountries();
-      setCountries(countryNames);
-    };
-
-    getCountries();
-  }, []);
-
-  function handleSelectChange(event) {
-    setSelectedCountry(event);
-  }
-
+export default function FormDropdown({ children, value, onChange }) {
   return (
-    <Listbox value={selectedCountry} onChange={handleSelectChange}>
+    <Listbox value={value} onChange={onChange}>
       <div className="relative">
         <ListboxButton className="relative w-full cursor-default rounded-md bg-white/10 p-3 font-medium text-white/80 hover:bg-white/20 focus:bg-white/30 focus:outline-none focus:ring-0">
           <span className="flex items-center">
             <span
-              className={`${selectedCountry ? "text-white/80" : "text-white/50"} block truncate text-lg`}>
-              {selectedCountry ? selectedCountry : "Country"}
+              className={`${value ? "text-white/80" : "text-white/50"} block truncate text-base`}>
+              {value ? value.name : "Country"}
             </span>
           </span>
           <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -47,21 +22,8 @@ export default function FormDropdown() {
 
         <ListboxOptions
           transition
-          className="mt-1 max-h-56 w-full overflow-auto rounded-md bg-white/10 text-lg text-white/80 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in">
-          {countries
-            .sort((a, b) => ("" + a.name.common).localeCompare(b.name.common))
-            .map((country, index) => (
-              <ListboxOption
-                key={index}
-                value={country.name.common}
-                className="group relative cursor-pointer select-none p-3 text-white/80 data-[focus]:bg-white/30 data-[focus]:text-white">
-                <div className="flex items-center">
-                  <span className="block truncate font-medium group-data-[selected]:font-semibold">
-                    {country.name.common}
-                  </span>
-                </div>
-              </ListboxOption>
-            ))}
+          className="mt-1 max-h-56 w-full overflow-auto rounded-md bg-white/10 text-base text-white/80 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in">
+          {children}
         </ListboxOptions>
       </div>
     </Listbox>
--- src/components/Forms/Profile/index.jsx
+++ src/components/Forms/Profile/index.jsx
@@ -1,9 +1,32 @@
-import Input from "@/components/Input";
+"use client";
+
+import { useState, useEffect } from "react";
+
 import FormCheckbox from "../FormCheckbox";
 import FormDropdown from "../FormDropdown";
 import FormInput from "../FormInput";
+import InputField from "@/components/InputField";
+
+import { fetchCountries } from "@/utils/fetchCountries";
+import { ListboxOption } from "@headlessui/react";
 
 export default function ProfileForm() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const countryNames = await fetchCountries();
+      setCountries(countryNames);
+    };
+
+    getCountries();
+  }, []);
+
+  function handleSelectChange(event) {
+    setSelectedCountry(event);
+  }
+
   return (
     <form
       autoComplete="true"
@@ -13,7 +36,22 @@ export default function ProfileForm() {
         <FormInput placeholder={"First name"} id={"firstname"} />
         <FormInput placeholder={"Last name"} id={"lastname"} />
         <FormInput placeholder={"Email"} id={"email"} />
-        <FormDropdown />
+        <FormDropdown value={selectedCountry} onChange={handleSelectChange}>
+          {countries
+            .sort((a, b) => ("" + a.name.common).localeCompare(b.name.common))
+            .map((countries, index) => (
+              <ListboxOption
+                key={index}
+                value={countries.name.common}
+                className="group relative cursor-pointer select-none p-3 text-white/80 data-[focus]:bg-white/30 data-[focus]:text-white">
+                <div className="flex items-center">
+                  <span className="block truncate font-medium group-data-[selected]:font-semibold">
+                    {countries.name.common}
+                  </span>
+                </div>
+              </ListboxOption>
+            ))}
+        </FormDropdown>
       </div>
       <div className="flex flex-col gap-6">
         <p>What is your main activity/activities?</p>
@@ -25,7 +63,7 @@ export default function ProfileForm() {
           <FormCheckbox id={"Medical"} />
           <FormCheckbox id={"Inspections"} />
         </div>
-        <Input placeholder={"Other"} id={"other"} />
+        <InputField placeholder={"Other"} id={"other"} isLabelled={false} />
       </div>
       <div className="flex flex-col gap-6">
         <p>User handle and password</p>
