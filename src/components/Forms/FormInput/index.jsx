e084248c5f0d563a4296b1d90b981ad813c162b4
Evandro Costa <evandro.souzac@gmail.com>
2024-08-12 14:31:28 -0300

    refactor: change props of the component to make more reusable

--- src/components/Forms/FormInput/index.jsx
+++ src/components/Forms/FormInput/index.jsx
@@ -1,12 +1,18 @@
-export default function FormInput({ id, placeholder, type }) {
+export default function FormInput({ label, isLabelled, type, ...props }) {
   return (
     <>
-      <label className="sr-only" htmlFor={id}></label>
+      {label && (
+        <label
+          className={`${isLabelled ? "text-center text-sm text-white" : "sr-only"}`}
+          htmlFor={props.id}>
+          {label}
+        </label>
+      )}
       <input
-        id={id}
-        placeholder={placeholder}
+        id={props.id}
+        placeholder={props.placeholder}
         type={type ? type : "text"}
-        className="w-full rounded-md border-none bg-white/10 p-3 text-lg font-medium text-white/80 outline-0 placeholder:text-lg placeholder:text-white/50 hover:bg-white/20 focus:bg-white/30 focus:outline-none focus:ring-0"
+        className="w-full rounded-md border-none bg-white/10 p-3 text-base font-medium text-white/80 outline-0 placeholder:text-base placeholder:text-white/50 hover:bg-white/20 focus:bg-white/30 focus:outline-none focus:ring-0"
       />
     </>
   );
