9bb0d5d2925cba047a992b1119cd1ed85dbcc298
Evandro Costa <evandro.souzac@gmail.com>
2024-08-12 17:56:57 -0300

    style: add font weight and font color to button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,8 @@ export default function Button({ ...props }) {
       type={props.type}
       form={props.form}
       disabled={props.disabled}
-      className={`rounded-3xl ${props.background} px-5 py-3 text-base uppercase ${props.disabled ? "disabled:opacity-20" : ""}`}>
+      className={`min-w-40 rounded-3xl ${props.background} px-5 py-3 text-base font-bold uppercase text-gray-dark ${props.disabled ? "disabled:opacity-20" : ""}`}>
+      {props.children}
       <span className="flex items-center justify-center gap-2">
         {props.icon ? props.icon : ""} {props.label}
       </span>
