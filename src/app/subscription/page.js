5e5eff9352f1b331ea05c2a18ec172778f1a4b72
Evandro Costa <evandro.souzac@gmail.com>
2024-08-13 19:15:28 -0300

    feat: create `/subscription` page

---
+++ src/app/subscription/page.js
@@ -0,0 +1,39 @@
+import Button from "@/components/Button";
+import Section from "@/components/Section";
+import { subscriptionsData } from "@/lib/subscriptionData";
+
+import CheckIcon from "@mui/icons-material/Check";
+
+export default function Subscription() {
+  return (
+    <Section>
+      <div className="mt-10 flex flex-col gap-10">
+        <div className="flex flex-col gap-6">
+          <p className="text-sm font-medium text-gray">
+            You have access to <b className="text-gray-light">PACTTO PRO </b>as
+            a trial.
+            <b className="text-gray-light"> 1 days left</b>
+          </p>
+          <div className="w-full">
+            <Button
+              type={"button"}
+              background={"bg-gray"}
+              label={"Subscribe to pactto Pro"}
+            />
+          </div>
+          <p className="text-base font-medium text-gray">
+            The PACTTO PRO subscription allows you to:
+          </p>
+        </div>
+        <ul className="text-base font-medium leading-loose text-gray">
+          {subscriptionsData.map((data) => (
+            <li className="flex items-center gap-3" key={data.id}>
+              <CheckIcon className="h-5 w-5 text-yellow" />
+              {data.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+}
---
+++ src/lib/subscriptionData.js
@@ -0,0 +1,69 @@
+export const subscriptionsData = [
+  {
+    id: 1,
+    text: "Every feature on the FREE plan",
+  },
+  {
+    id: 2,
+    text: "Full access to the Pactto app on iPhones, iPads, Android, Mac, Windows, and Web",
+  },
+  {
+    id: 3,
+    text: (
+      <>
+        Max screen recording duration on Pactto Desktop: <b>unlimited</b>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    text: (
+      <>
+        Max review duration on mobile: <b>unlimited</b>
+      </>
+    ),
+  },
+  {
+    id: 5,
+    text: "Max review duration on Pactto Web: 12 minutes",
+  },
+  {
+    id: 6,
+    text: "Max review duration on Pactto Desktop: 20 minutes",
+  },
+  {
+    id: 7,
+    text: "Manage and use your reference video library on the Web and Desktop",
+  },
+  {
+    id: 8,
+    text: "Review videos uploaded by you, using the browser",
+  },
+  {
+    id: 9,
+    text: "Add your own logo to create branded reviews",
+  },
+  {
+    id: 10,
+    text: "If you sell reviews, the Pactto take rate for paid reviews is 3%, instead of 12% for free users",
+  },
+  {
+    id: 11,
+    text: (
+      <>
+        <a
+          href="https://pactto.mintlify.app/introduction"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-yellow">
+          Integrate Pactto to your website
+        </a>
+        so anyone can send, and pay, for your video feedback
+      </>
+    ),
+  },
+  {
+    id: 12,
+    text: "Share your videos with others (no more sending video files around)",
+  },
+];
