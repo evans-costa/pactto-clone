013306ccd21b43dcaca83a85e069084b2d3a19fa
Evandro Costa <evandro.souzac@gmail.com>
2024-08-13 19:11:20 -0300

    style: change padding and font size in sidebar menu

--- src/components/MenuList/index.jsx
+++ src/components/MenuList/index.jsx
@@ -9,7 +9,7 @@ export default function MenuList() {
 
   const listItems = menuData.map((item) => (
     <li
-      className={`px-8 py-4 ${pathname === item.path ? "bg-[#3a363f] text-yellow" : ""} hover:bg-[#3a363f] hover:text-yellow`}
+      className={`px-8 py-3 ${pathname === item.path ? "bg-[#3a363f] text-yellow" : ""} hover:bg-[#3a363f] hover:text-yellow`}
       key={item.id}>
       <Link href={item.path}>
         <span className="flex items-center gap-4">
@@ -19,7 +19,7 @@ export default function MenuList() {
     </li>
   ));
   return (
-    <ul className="mt-8 flex flex-col text-base font-medium text-gray">
+    <ul className="mt-4 flex flex-col text-sm font-medium text-gray">
       {listItems}
     </ul>
   );
