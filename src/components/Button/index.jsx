26ced28ba3d57815fc2440e78df2853753ee20c4
Evandro Costa <evandro.souzac@gmail.com>
2024-08-13 19:12:05 -0300

    style: add a `OnClick` props and change some styles

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,8 +3,9 @@ export default function Button({ ...props }) {
     <button
       type={props.type}
       form={props.form}
+      onClick={props.onClick}
       disabled={props.disabled}
-      className={`min-w-40 rounded-3xl ${props.background} px-5 py-3 text-base font-bold uppercase text-gray-dark ${props.disabled ? "disabled:opacity-20" : ""}`}>
+      className={`min-w-44 max-w-fit rounded-3xl ${props.background} px-5 py-3 text-base font-bold uppercase text-gray-dark ${props.disabled ? "disabled:opacity-20" : ""}`}>
       {props.children}
       <span className="flex items-center justify-center gap-2">
         {props.icon ? props.icon : ""} {props.label}
