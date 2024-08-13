82b86c2210274a27807479844d691d29794abadd
Evandro Costa <evandro.souzac@gmail.com>
2024-08-13 19:10:46 -0300

    style: add a default checked props on component

--- src/components/Forms/FormCheckbox/index.jsx
+++ src/components/Forms/FormCheckbox/index.jsx
@@ -1,8 +1,9 @@
-export default function FormCheckbox({ id }) {
+export default function FormCheckbox({ id, defaultChecked }) {
   return (
     <div className="flex items-center gap-5">
       <input
         type="checkbox"
+        defaultChecked={defaultChecked}
         id={id}
         className="inset-0 z-20 h-5 w-5 cursor-pointer rounded border-2 border-gray bg-transparent text-yellow focus:ring-yellow"></input>
       <label htmlFor={id} className="cursor-pointer">
