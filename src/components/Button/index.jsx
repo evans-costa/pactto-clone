58cc0ab616fb497faeecbf942dab611655ca65dc
Evandro Costa <evandro.souzac@gmail.com>
2024-08-14 16:11:07 -0300

    style: add opacity on hover in `Button` component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,7 +5,7 @@ export default function Button({ ...props }) {
       form={props.form}
       onClick={props.onClick}
       disabled={props.disabled}
-      className={`min-w-44 max-w-fit rounded-3xl ${props.background} px-5 py-3 text-base font-bold uppercase text-gray-dark ${props.disabled ? "disabled:opacity-20" : ""}`}>
+      className={`min-w-44 max-w-fit rounded-3xl ${props.background} px-5 py-3 text-base font-bold uppercase text-gray-dark hover:opacity-90 ${props.disabled ? "disabled:opacity-20" : ""}`}>
       {props.children}
       <span className="flex items-center justify-center gap-2">
         {props.icon ? props.icon : ""} {props.label}
